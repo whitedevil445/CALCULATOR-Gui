b6335038a7afead103642290377c0c13a047420e
whitedevil445 <106152183+whitedevil445@users.noreply.github.com>
2023-09-12 18:10:52 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function clk(val){
+
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function clrdisp(){
+    document.getElementById("screen").value=""
+}
+function eql(){
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value=result
+}
